6b4997009b16317ddc7c22701e4395b543ca4457
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2021-03-12 11:29:55 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,3 @@
 terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    helm = {
-      source = "hashicorp/helm"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-  }
+  required_version = ">= 0.12"
 }
