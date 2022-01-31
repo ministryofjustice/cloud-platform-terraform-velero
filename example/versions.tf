b79ba0cdb59c39d39c958d1935370628bc8c803e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-01-31 10:37:15 +0000

    really add test

---
+++ example/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
